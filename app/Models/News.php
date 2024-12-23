9121fc96b5d83fcb1c85556dcf43f8ab00845423
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2024-12-23 18:04:02 +0100

    Update News.php

--- app/Models/News.php
+++ app/Models/News.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'image_path',
+        'publication_date',
+    ];
 }
